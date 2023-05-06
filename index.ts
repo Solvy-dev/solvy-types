27c717537bb229bcbc79597cedb3ef5baae50d90
German Tellez <german.tellez@xa.epicgames.com>
2023-05-06 15:15:04 -0500

    Update entry file

--- index.ts
+++
@@ -1,7 +0,0 @@
-export * from './books';
-export * from './categories';
-export * from './problems';
-export * from './requests';
-export * from './sections';
-export * from './solutions';
-export * from './users';
