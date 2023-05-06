35f0632e3528af3001328fdc60df62ebaa85cd30
German Tellez <german.tellez@xa.epicgames.com>
2023-05-06 16:07:51 -0500

    Update solution interface

--- solutions/index.ts
+++ solutions/index.ts
@@ -11,7 +11,11 @@ export interface SolutionDB extends Solution {
   createdAt: string;
 }
 
-export type UpdateSolutionPayload = Pick<SolutionDB, 'description' | 'picture' | 'author'>;
+export interface UpdateSolutionPayload {
+  author?: string;
+  description?: string;
+  picture?: string;
+}
 
 export interface AddSolution {
   (solution: Solution): Promise<SolutionDB>
