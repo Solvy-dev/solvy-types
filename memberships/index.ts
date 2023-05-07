d2ea9865e275aa76d6422b5f3431f9cbd12278ab
German Tellez <german.tellez@xa.epicgames.com>
2023-05-07 13:22:35 -0500

    Update membership

--- memberships/index.ts
+++ memberships/index.ts
@@ -14,13 +14,13 @@ export interface Membership {
   userId:     string;
   type:       MembershipType;
   status:     MembershipStatus;
-  startDate?:  string;
-  endDate?:    string;
 }
 
 export interface MembershipDB extends Membership {
   id:         string;
   createdAt:  string;
+  startDate?:  string;
+  endDate?:    string;
 }
 
 export type UpdateMemership = Omit<Membership, 'userId' | 'email'>
