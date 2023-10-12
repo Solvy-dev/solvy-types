55bec828df57b26fae3d9d92f5eb4239c90d176e
German Tellez <gertellezv@gmail.com>
2023-10-12 15:17:20 -0500

    add ProblemSeedPayload

--- problems/index.ts
+++ problems/index.ts
@@ -1,3 +1,5 @@
+import { AttachmentPayload } from '@models/shared';
+
 export interface Problem {
   bookId: string;
   chapterId: string;
@@ -6,16 +8,16 @@ export interface Problem {
   picture?: string;
   position: number;
 }
-
 export interface ProblemDB extends Problem {
   id: string;
   slug: string;
   createdAt: string;
   updatedAt: string;
 }
-
 export type ProblemSeed = Omit<ProblemDB, 'createdAt' | 'updatedAt'>;
-
+export type ProblemSeedPayload = Omit<ProblemSeed, 'picture'> & {
+  picture?: AttachmentPayload;
+};
 export type UpdateProblemPayload = Partial<
   Pick<ProblemDB, 'name' | 'description' | 'picture' | 'slug' | 'position'>
 >;
