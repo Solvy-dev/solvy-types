0fc2fd37dd1c4d84c180a168cfd99fd9b6a9bda3
German Tellez <gertellezv@gmail.com>
2023-10-15 16:00:14 -0500

    Update UpdateChapterPayload

--- chapters/index.ts
+++ chapters/index.ts
@@ -13,11 +13,10 @@ export interface ChapterDB extends Chapter {
   updatedAt: string;
 }
 export type ChapterSeed = Omit<ChapterDB, 'createdAt' | 'updatedAt'>;
-export interface UpdateChapterPayload {
-  title?: string;
-  subtitle?: string;
-  position?: number;
-}
+export type UpdateChapterPayload = Pick<
+  ChapterDB,
+  'title' | 'subtitle' | 'position' | 'updatedAt'
+>;
 
 export interface ChapterWithProblems extends Chapter {
   problems: ProblemDB[];
