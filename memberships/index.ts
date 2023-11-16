3dedc2e39af7760a081053f5ea1fddd5f0f2ce7a
German Tellez <german.tellez@xa.epicgames.com>
2023-11-15 19:19:48 -0500

    Update membership

--- memberships/index.ts
+++ memberships/index.ts
@@ -1,28 +1,16 @@
-export enum MembershipType {
-  FREE = 'FREE',
-  BASIC = 'BASIC',
-  PREMIUM = 'PREMIUM',
-}
-
-export enum MembershipStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
-  CANCELLED = 'CANCELLED',
-}
-
 export interface Membership {
   userId: string;
-  type: MembershipType;
-  status: MembershipStatus;
+  type: 'FREE' | 'PREMIUM';
+  status: 'ACTIVE' | 'INACTIVE' | 'CANCELLED';
+  startDate?: string;
+  endDate?: string;
 }
 
 export interface MembershipDB extends Membership {
   id: string;
   createdAt: string;
-  startDate?: string;
-  endDate?: string;
 }
 
-export type UpdateMemership = Partial<
+export type UpdateMembership = Partial<
   Pick<MembershipDB, 'type' | 'status' | 'startDate' | 'endDate'>
 >;
