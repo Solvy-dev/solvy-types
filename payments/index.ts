b460d0d2dbe7941a0976107faa76b66000214f0f
German Tellez <gertellezv@gmail.com>
2024-01-20 19:52:27 -0500

    Add OpenpayWebhook type

--- payments/index.ts
+++ payments/index.ts
@@ -7,3 +7,55 @@ export type AddSubscriptionPayload = {
   expiration: string;
   plan: string;
 };
+
+// OpenPay
+export interface OpenpayWebhook {
+  type: 'charge.succeeded' | 'subscription.charge.failed'
+  event_date: string;
+  transaction: Transaction;
+}
+
+export interface Transaction {
+  id: string;
+  authorization: string;
+  operation_type: string;
+  transaction_type: string;
+  status: string;
+  conciliated: boolean;
+  iva: string;
+  creation_date: string;
+  operation_date: string;
+  description: string;
+  error_message: null;
+  order_id: null;
+  card: Card;
+  customer_id: string;
+  subscription_id: string;
+  fee: Fee;
+  amount: number;
+  currency: string;
+  method: string;
+}
+
+export interface Card {
+  id: string;
+  type: string;
+  brand: string;
+  address: null;
+  card_number: string;
+  holder_name: string;
+  expiration_year: string;
+  expiration_month: string;
+  allows_charges: boolean;
+  allows_payouts: boolean;
+  creation_date: string;
+  bank_name: string;
+  bank_code: string;
+  customer_id: string;
+}
+
+export interface Fee {
+  amount: number;
+  tax: number;
+  currency: string;
+}
