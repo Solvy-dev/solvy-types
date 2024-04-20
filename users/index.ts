e4ee26ac13213b66a6b5eeb92780746014a13aac
German Tellez <gertellezv@gmail.com>
2024-04-19 22:57:02 -0500

    Update user types

--- users/index.ts
+++ users/index.ts
@@ -1,26 +1,12 @@
 import { CalendarDate } from "@internationalized/date";
 
-export type UserProfileForm = {
-  firstName: string;
-  picture?: string;
-  lastName?: string;
-  birthDate?: CalendarDate;
-  country?: string;
-  city?: string;
-};
-export type UserProfile = Omit<UserProfileForm, 'birthDate'> & { birthDate?: string };
-
 export type User = {
+  id: string;
   email: string;
-  username: string;
-  profile: UserProfile;
-  isVerified: boolean;
-  accountType: string; // 'credentials' | 'oauth'
-  password?: string;
-  provider: string;
+  profile: UserProfile,
   type: 'student' | 'tutor' | 'parent'; // Analytics
   role: 'student' | 'tutor' | 'admin'; // Show content conditionally in the platform
-};
+}
 
 export type UserDB = User & {
   id: string;
@@ -28,19 +14,14 @@ export type UserDB = User & {
   updatedAt: string;
 };
 
-export type UpdateUserProfile = {
-  profile: UserProfile;
-  updatedAt: string;
+export type UserProfile = {
+  username: string;
+  firstName?: string;
+  picture?: string;
+  lastName?: string;
+  birthDate?: string;
+  country?: string;
+  city?: string;
 };
-export interface NextAuthUser {
-  name: string;
-  image: string;
-  email: string;
-}
 
-export type ChangePassword = {
-  currentPassword: string;
-  newPassword: string;
-  confirmPassword: string;
-  token: string;
-};
+export type UserProfileForm = Omit<UserProfile, 'birthDate'> & { birthDate?: CalendarDate };
