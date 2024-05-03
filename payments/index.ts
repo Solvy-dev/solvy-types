c813596a798f1d0a80024037ddfe0f16dfa14099
German Tellez <gertellezv@gmail.com>
2024-05-03 17:54:15 -0500

    add openpay webhook event type

--- payments/index.ts
+++ payments/index.ts
@@ -1,78 +1,35 @@
+import { OpenPayCard } from '../openpay/webhook'
+
 export type AddSubscriptionPayload = {
-  nid: string;
-  userId: string;
-  email: string;
-  cardName: string;
-  cardNumber: string;
-  cardCVV: string;
-  expiration: string;
-  plan: string;
-};
+  nid: string
+  userId: string
+  email: string
+  cardName: string
+  cardNumber: string
+  cardCVV: string
+  expiration: string
+  plan: string
+}
 
 export type PaymentHistory = {
-  userId: string;
-  planId: string;
-  membershipId: string;
-  status: string;
-  paymentMethodUsed: string;
-  paymentDate: string;
-  amount: number;
+  userId: string
+  planId: string
+  membershipId: string
+
+  status: string
+  paymentGateway: 'OPENPAY'
+  customerId: string
+  subscriptionId: string
+
+  amount: number
+  currency: string
+  method: string
+  card: OpenPayCard
+  eInvoiceId?: string // Electronic Invoice required in Colombia
 }
 
 export type PaymentHistoryDB = PaymentHistory & {
-  id: string;
-  eInvoiceId: string; // Electronic Invoice required in Colombia
-  createdAt: string;
-}
-
-// OpenPay
-export interface OpenpayWebhook {
-  type: 'charge.succeeded' | 'charge.refunded' | 'subscription.charge.failed'
-  event_date: string;
-  transaction: Transaction;
-}
-
-export interface Transaction {
-  id: string;
-  authorization: string;
-  operation_type: string;
-  transaction_type: string;
-  status: string;
-  conciliated: boolean;
-  iva: string;
-  creation_date: string;
-  operation_date: string;
-  description: string;
-  error_message: null;
-  order_id: null;
-  card: Card;
-  customer_id: string;
-  subscription_id: string;
-  fee: Fee;
-  amount: number;
-  currency: string;
-  method: string;
-}
-
-export interface Card {
-  id: string;
-  type: string;
-  brand: string;
-  address: null;
-  card_number: string;
-  holder_name: string;
-  expiration_year: string;
-  expiration_month: string;
-  allows_charges: boolean;
-  allows_payouts: boolean;
-  creation_date: string;
-  bank_name: string;
-  bank_code: string;
-  customer_id: string;
-}
-
-export interface Fee {
-  amount: number;
-  tax: number;
-  currency: string;
+  id: string
+  createdAt: string
+  updatedAt: string
 }
